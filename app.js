f11c03ca4fdcfb95ab2550ea7c113c3056b1650d
dhananjaybelhekar <dhananjay.b@adnatesolutions.com>
2018-07-19 14:49:35 +0530

    changes socket path

--- app.js
+++ app.js
@@ -7,12 +7,15 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', function(socket){
-   socket.on('event', function(data1,data2){
-   	console.log(data1,data2);
-   io.emit(data1,data2);
-   });
+var nsp = io.of('/cht');
+
+nsp.on('connection', function(socket){
+socket.on('event', function(data,msg){ nsp.emit(data,msg); });
 });
+
+	
+
+
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
